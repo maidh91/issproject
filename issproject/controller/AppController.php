8b0650cc98444cb33e6f80fe05d50140e39ca7b6
ctt.bk.hcmut2009@gmail.com <ctt.bk.hcmut2009@gmail.com@bf2ca782-db8c-56f6-9f0d-7f205502ede7>
2012-12-10 12:11:51 +0000

    sua xong

--- issproject/controller/AppController.php
+++ issproject/controller/AppController.php
@@ -1,6 +1,11 @@
 <?php
 class AppController extends  Controller{
-    public $layout = "templates/layout/app.html";
+    public $layout = "{ROOT_DIR}/templates/layout/app.html";
+    function __construct(){
+	parent::__construct();
+        // set layout
+        $this->smarty->assign('layout',$this->layout);        
+    }
 }
 /*
  * To change this template, choose Tools | Templates
